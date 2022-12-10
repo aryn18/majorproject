d8eca5f1c905a8cfd9453ffbc7d39a62a2d39512
Aryan <aryan.gupta_cs19@gla.ac.in>
2022-12-10 11:00:39 +0530

    Working on customer side

--- core/views.py
+++ core/views.py
@@ -1,19 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect, render
 from django.contrib.auth import login
-
-from django.contrib.auth.decorators import login_required
 
 from . import forms
 # Create your views here.
 def home(request):
-    return render (request, 'home.html')
-
-@login_required
-def customer_page(request):
-    return render (request, 'home.html')
-
-@login_required
-def courier_page(request):
     return render (request, 'home.html')
 
 def sign_up(request):
@@ -34,3 +24,4 @@ def sign_up(request):
     return render (request, 'sign_up.html',{
         'form': form
     })
+    
--- dropship/urls.py
+++ dropship/urls.py
@@ -3,6 +3,20 @@ from django.urls import path, include
 from core import views
 from django.contrib.auth import views as auth_views
 
+
+from core.customer import views as customer_view;
+from core.courier import views as courier_view;
+
+customer_urlpatterns=[
+    path('', customer_view.home, name='home'),
+    path('profile/', customer_view.profile_page, name="profile"),
+
+]
+
+courier_urlpatterns=[
+    path('', courier_view.home, name='home')
+]
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home),
@@ -14,7 +28,7 @@ urlpatterns = [
     path('sign-out/', auth_views.LogoutView.as_view(next_page="/")),
     path('sign-up/', views.sign_up),
 
-    path('customer/', views.customer_page),
-    path('courier/', views.courier_page),
+    path('customer/', include((customer_urlpatterns, 'customer'))),
+    path('courier/', include((courier_urlpatterns, 'courier'))),
 
 ]
